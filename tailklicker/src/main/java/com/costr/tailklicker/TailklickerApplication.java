51e79be2a13ac30311e9c8e3fbce805c5837c556
Fibi <76633686+Stinki3001@users.noreply.github.com>
2025-06-13 11:42:25 +0200

    Add files via upload

---
+++ tailklicker/src/main/java/com/costr/tailklicker/TailklickerApplication.java
@@ -0,0 +1,13 @@
+package com.costr.tailklicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailklickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TailklickerApplication.class, args);
+	}
+
+}
