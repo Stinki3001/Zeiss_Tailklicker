39bb48e145f625a611f88149762bfe29f2d6c32d
Fietje Costrau <costraufietje@gmail.com>
2025-06-16 10:55:34 +0200

    Version 1.0.1-SNAPSHOT build

--- tailklicker/src/main/java/com/costr/tailklicker/TailklickerApplication.java
+++ tailklicker/src/main/java/com/costr/tailklicker/TailklickerApplication.java
@@ -5,28 +5,32 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import com.costr.tailklicker.GUI.SwingGUI;
+
 /**
  * Tailklicker Application
+ *
  * @author Costr
  */
 @SpringBootApplication
 public class TailklickerApplication {
 
-	public static void main(String[] args) {
-		 System.setProperty("java.awt.headless", "false");
-		SpringApplication.run(TailklickerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.setProperty("java.awt.headless", "false");
+        SpringApplication.run(TailklickerApplication.class, args);
+    }
 
-	 @Bean
+    @Profile("gui")
+    @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-			SwingGUI gui = new SwingGUI();
-			int rows = 3; 
+            SwingGUI gui = new SwingGUI();
+            int rows = 3;
             int cols = 3;
-			gui.init(rows, cols);
-            
+            gui.init(rows, cols);
+
         };
     }
 
