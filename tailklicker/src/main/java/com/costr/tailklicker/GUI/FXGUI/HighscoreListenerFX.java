91e3fbd35f33be5db9b187a0aaa9f02544d226f4
Fietje Costrau <costraufietje@gmail.com>
2025-06-23 08:04:15 +0200

    formater

--- tailklicker/src/main/java/com/costr/tailklicker/GUI/FXGUI/HighscoreListenerFX.java
+++ tailklicker/src/main/java/com/costr/tailklicker/GUI/FXGUI/HighscoreListenerFX.java
@@ -1,6 +1,5 @@
 package com.costr.tailklicker.GUI.FXGUI;
 
-
 import java.util.Set;
 import java.util.logging.Level;
 
@@ -17,46 +16,48 @@ public class HighscoreListenerFX implements Notation {
 
         HighscoreFrameFX highscoreScene;
 
-    
-    public void actionPerformed() {
-        highscoreScene = new HighscoreFrameFX();
-        Set<Player> playerList = TailklickerApplication.getPlayerList();
-        Player currentPlayer = TailklickerApplication.getPlayer();
+        public void actionPerformed() {
+                highscoreScene = new HighscoreFrameFX();
+                Set<Player> playerList = TailklickerApplication.getPlayerList();
+                Player currentPlayer = TailklickerApplication.getPlayer();
 
-        LOGGER.log(Level.INFO, "{0}HighscoreListener: {1} Spieler im Highscore{2}",
-                new Object[] { BLAU, playerList.size(), RESET });
-        Boolean add = false;
-        outer:
-            for (Player oldPlayer : playerList) {
-                if (oldPlayer.getName().equals(currentPlayer.getName())
-                 && oldPlayer.getLevel().toString().equalsIgnoreCase(currentPlayer.getLevel().toString())) {
-                    LOGGER.log(Level.WARNING, "{0}Der Spieler {1} mit dem Level: {2} ist bereits im Highscore.{3}",
-                            new Object[] { YELLOW, currentPlayer.getName(),
-                                    currentPlayer.getLevel().toString().toUpperCase(), RESET });
-                    if (oldPlayer.getCount() > currentPlayer.getCount()) {
-                        LOGGER.log(Level.INFO, "{0}Der Spieler {1} hat einen besseren Score ereicht.{2}",
-                                new Object[] { BLAU, currentPlayer.getName(), RESET });
-                        oldPlayer.setCount(currentPlayer.getCount());
-                        LOGGER.log(Level.INFO, "{0}Highscore updated to {1}!{2}",
-                                new Object[] { BLAU, currentPlayer.getCount(), RESET });
-                        break outer;
+                LOGGER.log(Level.INFO, "{0}HighscoreListener: {1} Spieler im Highscore{2}",
+                                new Object[] { BLAU, playerList.size(), RESET });
+                Boolean add = false;
+                outer: for (Player oldPlayer : playerList) {
+                        if (oldPlayer.getName().equals(currentPlayer.getName())
+                                        && oldPlayer.getLevel().toString()
+                                                        .equalsIgnoreCase(currentPlayer.getLevel().toString())) {
+                                LOGGER.log(Level.WARNING,
+                                                "{0}Der Spieler {1} mit dem Level: {2} ist bereits im Highscore.{3}",
+                                                new Object[] { YELLOW, currentPlayer.getName(),
+                                                                currentPlayer.getLevel().toString().toUpperCase(),
+                                                                RESET });
+                                if (oldPlayer.getCount() > currentPlayer.getCount()) {
+                                        LOGGER.log(Level.INFO,
+                                                        "{0}Der Spieler {1} hat einen besseren Score ereicht.{2}",
+                                                        new Object[] { BLAU, currentPlayer.getName(), RESET });
+                                        oldPlayer.setCount(currentPlayer.getCount());
+                                        LOGGER.log(Level.INFO, "{0}Highscore updated to {1}!{2}",
+                                                        new Object[] { BLAU, currentPlayer.getCount(), RESET });
+                                        break outer;
 
-                } else {
-                    continue;
+                                } else {
+                                        continue;
+                                }
+
+                        }
+                        add = true;
                 }
+                if (add) {
+                        LOGGER.log(Level.INFO, "{0}Der Spieler {1} ist nicht im Highscore.{2}",
+                                        new Object[] { BLAU, TailklickerApplication.getPlayer().getName(), RESET });
+                        TailklickerApplication.addPlayerToList(TailklickerApplication.getPlayer());
+                        LOGGER.log(Level.INFO, "{0}Der Spieler {1} wurde zum Highscore hinzugefügt.{2}",
+                                        new Object[] { BLAU, TailklickerApplication.getPlayer().getName(), RESET });
+                }
+                highscoreScene.dispalyHighscore(playerList);
+                highscoreScene.write(playerList);
 
-            }
-            add = true;
         }
-        if(add){
-                LOGGER.log(Level.INFO, "{0}Der Spieler {1} ist nicht im Highscore.{2}",
-                    new Object[] { BLAU, TailklickerApplication.getPlayer().getName(), RESET });
-            TailklickerApplication.addPlayerToList(TailklickerApplication.getPlayer());
-            LOGGER.log(Level.INFO, "{0}Der Spieler {1} wurde zum Highscore hinzugefügt.{2}",
-                    new Object[] { BLAU, TailklickerApplication.getPlayer().getName(), RESET });
-        }
-        highscoreScene.dispalyHighscore(playerList);
-        highscoreScene.write(playerList);
-
-    }
 }
